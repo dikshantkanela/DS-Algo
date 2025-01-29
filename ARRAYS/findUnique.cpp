f4fb3faea7d035f6c0693dff813834a1610b36c5
dikshantkanela <dikshant7156@gmail.com>
2025-01-30 02:16:53 +0530

    swapAlternate elements xor

--- ARRAYS/findUnique.cpp
+++ ARRAYS/findUnique.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int findUnique(int *arr, int size)
+
+int findUniqueBruteForce(int *arr, int size)
 {
     
     for(int i = 0; i<size;i++){
@@ -17,8 +18,20 @@ int findUnique(int *arr, int size)
     }
     return -1;
 }
+
+int findUniqueOptimise(int arr[], int size){ // can be optimised by takig xor, only the unique element will be left,rest all the elements will get cancelled due to xor operation
+     int res = 0;
+     for(int i=0;i<size;i++){
+        res = res ^ arr[i]; 
+     }
+    return res;
+}
+
 int main(){
-    int arr[5] = {2,2,2,3,4};
-    cout<<findUnique(arr,5);
+    int arr[7] = {2, 3, 1, 6, 3, 6, 2}; 
+    cout<<findUniqueBruteForce(arr,7);
+    cout<<endl;
+    cout<<findUniqueOptimise(arr,7);
+
     return 0;
 }
