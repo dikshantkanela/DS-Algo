49a837a5a84cd0ae7b6fa8965c75e652c3ba88c3
dikshantkanela <dikshant7156@gmail.com>
2025-02-08 20:18:40 +0530

    sq(x)leetcode

---
+++ BINARY SEARCH/sq(x).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int mySqrt(int x) {
+    int start = 0, end = x;
+    int ans = -1; 
+    long long mid = start + (end - start) / 2; // Avoid overflow
+    while (start <= end) {
+        
+        long long sq = mid * mid;
+
+        if (sq > x) {
+            end = mid - 1;
+        } 
+        else if (sq < x) { // Mid is a potential answer
+            ans = mid;
+            start = mid + 1;
+        } 
+        else {
+            return mid; // Perfect square found
+        }
+    }
+    return ans; 
+}
+
+int main() {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Square root (integer part): " << mySqrt(x) << endl;
+    return 0;
+}
