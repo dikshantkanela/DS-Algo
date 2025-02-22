c044bb4e77661ee6689d9e2836e864b9f6926ed3
dikshantkanela <dikshant7156@gmail.com>
2025-02-22 18:34:43 +0530

    replace spaces

---
+++ STRINGS/replaceSpaces@40.cpp
@@ -0,0 +1,33 @@
+// Approach:
+// 1. Iterate through the given string character by character.
+// 2. If a space (' ') is encountered, replace it with "@40".
+// 3. Otherwise, append the character to the result string.
+// 4. Return the modified string after processing.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to replace spaces with "@40"
+string replaceSpaces(string &str) {
+    int n = str.length();
+    string word = "";
+
+    for (int i = 0; i < n; i++) {
+        while (str[i] != ' ' && i < n) { // Process normal characters and get a word
+            word += str[i];
+            i++;
+        }
+        if (str[i] == ' ') { // Replace space with "@40"
+            word += "@40";
+        }
+    }
+    return word;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "Modified string: " << replaceSpaces(str) << endl;
+    return 0;
+}
