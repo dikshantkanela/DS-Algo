3f53978abd4f176934b158e8048a30e1a78f4c5a
dikshantkanela <dikshant7156@gmail.com>
2025-02-22 19:52:01 +0530

    remove substrings allll

---
+++ STRINGS/RemoveSubstringOccurences.cpp
@@ -0,0 +1,22 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to remove all occurrences of "part" from "s"
+string removeOccurrences(string s, string part) {
+    while (s.find(part) != string::npos) {
+        s.erase(s.find(part), part.length());// IF THE PART IS NOT FOUND THEN THE LOOP WILL BREAK SINCE ALL THE SUBSTR PARTS ARE ALREADY REMOVED!
+    }
+    return s;
+}
+
+int main() {
+    string s, part;
+    cout << "Enter the main string: ";
+    cin >> s;
+    cout << "Enter the part to remove: ";
+    cin >> part;
+    cout << "Modified string: " << removeOccurrences(s, part) << endl;
+    return 0;
+}
