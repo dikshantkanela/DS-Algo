4d3f4794b838bbdd50b172393ffaebcd0ff7d240
dikshantkanela <dikshant7156@gmail.com>
2025-03-21 23:17:03 +0530

    inheritance all cases

---
+++ OOPS/inhertiance.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Human{
+    public:
+    int height;
+    int weight;
+    int age;
+    
+    public:
+    int getAge(){
+        return this->age;
+    }
+
+    void setWeight(int w){
+        this->weight = w;
+    }
+
+};
+
+class Male: private Human{
+    public:
+    string color;
+
+
+    void sleep(){
+       cout<<"Human Sleeping!";
+    }
+
+    int getHeight(){
+        return this->height;
+    }
+    void setHeight(int h){
+        this->height = h;
+    }
+
+};
+
+int main(){
+
+    Male m1;
+    cout<< m1.getHeight();
+
+
+    /* 
+    Male obj1;
+    // obj1.age = 20;
+    cout<<obj1.age<<endl;
+    cout<<obj1.height<<endl;
+    obj1.setWeight(57);
+    cout<<obj1.weight<<endl;
+    cout<<obj1.color<<endl;
+    obj1.sleep();
+    */
+   
+
+    return 0;
+}
