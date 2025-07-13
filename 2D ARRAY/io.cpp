f45d8993e787eedc5a37611a67853060bdd4501b
dikshantkanela <dikshant7156@gmail.com>
2025-07-13 22:05:59 +0530

    row sum of matrix

---
+++ 2D ARRAY/io.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3][4]; // 3 rows and 4 colums
+    // input
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<"Enter element of "<<i<<"row and "<<j<<"th column";
+            cin>>arr[i][j];
+        }
+    }
+    // output : row wise
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<< " ";
+        }
+        cout<<endl;
+    }
+
+    // output column wise : 
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<< arr[j][i];
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
